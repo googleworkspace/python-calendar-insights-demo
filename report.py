73ed4f79cc543c374c28da3715cfaf3b0e982eb0
Steve Bazyl <sbazyl@google.com>
2023-07-20 15:13:22 -0600

    Improve output formatting

--- report.py
+++ report.py
@@ -18,23 +18,25 @@ from datetime import timedelta
 
 import humanize
 
-def print_daily_totals(data):
+def print_daily_totals(data, label_width=15):
     """ Print insights in the form of date -> timedelta """
     cumulative_time = timedelta(minutes=0)
     for (day, minutes) in data.items():
         cumulative_time = cumulative_time + minutes
-        date = humanize.naturaldate(day)
+        label = F"{humanize.naturaldate(day)}:".ljust(label_width)
         total_time = humanize.precisedelta(minutes, minimum_unit='minutes')
-        print(f"{date}: {total_time}")
+        print(f"{label} {total_time}")
         total_time = humanize.precisedelta(cumulative_time, minimum_unit='minutes')
-    print(f"Total: {total_time}")
+    label = "Total:".ljust(label_width)
+    print(f"{label} {total_time}")
 
 
-def print_labeled_totals(data):
+def print_labeled_totals(data, label_width=30):
     """ Print insights in the form of string -> timedelta """
     for (label, minutes) in data.items():
+        formatted_label = f"{label}:".ljust(30) if label is not None else '(unknown):'.ljust(label_width)
         total_time = humanize.precisedelta(minutes, minimum_unit='minutes')
-        print(f"{label}: {total_time}")
+        print(f"{formatted_label} {total_time}")
 
 def print_report(data):
     """ Pretty-print known generated insights """
